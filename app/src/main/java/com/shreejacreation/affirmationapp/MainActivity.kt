beba226b6e49c130103b4e4af03f034f740b3aef
sachinsandbhor <sachinsandbhor@gmail.com>
2025-08-31 13:26:39 +0530

    Affirmation app. Basic list with card view.

---
+++ app/src/main/java/com/shreejacreation/affirmationapp/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.shreejacreation.affirmationapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.shreejacreation.affirmationapp.data.Affirmation
+import com.shreejacreation.affirmationapp.data.DataSource
+import com.shreejacreation.affirmationapp.ui.AffirmationCard
+import com.shreejacreation.affirmationapp.ui.theme.AffirmationAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AffirmationAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AffirmationsApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AffirmationsApp() {
+    val affirmationList = DataSource().loadAffirmation()
+    val layoutDirection = LocalLayoutDirection.current
+    Surface(
+        modifier = Modifier.fillMaxSize().statusBarsPadding().
+        padding(start = WindowInsets.safeDrawing.asPaddingValues().calculateStartPadding(layoutDirection),
+            end = WindowInsets.safeDrawing.asPaddingValues().calculateEndPadding(layoutDirection))
+    ){
+        AffirmationList(affirmationList)
+    }
+}
+
+@Composable
+fun AffirmationList(affirmationList: List<Affirmation>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(affirmationList) { affirmation ->
+            AffirmationCard(
+                affirmation = affirmation,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AffirmationAppTheme {
+        AffirmationsApp()
+    }
+}
---
+++ app/src/main/java/com/shreejacreation/affirmationapp/ui/AffirmationCard.kt
@@ -0,0 +1,50 @@
+package com.shreejacreation.affirmationapp.ui
+
+import android.content.res.Resources
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.shreejacreation.affirmationapp.R
+import com.shreejacreation.affirmationapp.data.Affirmation
+import com.shreejacreation.affirmationapp.ui.theme.AffirmationAppTheme
+
+@Composable
+fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
+    Card(modifier = modifier) {
+        Column {
+            Image(
+                painter = painterResource(affirmation.imageResourceId),
+                contentDescription = stringResource(affirmation.stringResourceId),
+                modifier = Modifier.fillMaxWidth().height(194.dp),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = LocalContext.current.getString(affirmation.stringResourceId),
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.headlineSmall
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AffirmationAppTheme {
+        AffirmationCard(Affirmation(R.string.affirmation1, R.drawable.image1))
+    }
+}
