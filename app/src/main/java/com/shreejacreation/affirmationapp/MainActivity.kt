4907ffa45a621689ac4b5b92c898e4a9b59e31f5
sachinsandbhor <sachinsandbhor@gmail.com>
2025-08-31 21:41:29 +0530

    Affirmation app. Theme and typography changes.

--- app/src/main/java/com/shreejacreation/affirmationapp/MainActivity.kt
+++ app/src/main/java/com/shreejacreation/affirmationapp/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.calculateEndPadding
@@ -11,20 +13,29 @@ import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.compose.AffirmationAppTheme
 import com.shreejacreation.affirmationapp.data.Affirmation
 import com.shreejacreation.affirmationapp.data.DataSource
 import com.shreejacreation.affirmationapp.ui.AffirmationCard
-import com.shreejacreation.affirmationapp.ui.theme.AffirmationAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,8 +44,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AffirmationAppTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize()
                 ) {
                     AffirmationsApp()
                 }
@@ -58,20 +68,50 @@ fun AffirmationsApp() {
 
 @Composable
 fun AffirmationList(affirmationList: List<Affirmation>, modifier: Modifier = Modifier) {
-    LazyColumn(modifier = modifier) {
-        items(affirmationList) { affirmation ->
-            AffirmationCard(
-                affirmation = affirmation,
-                modifier = Modifier.padding(8.dp)
-            )
+    Scaffold(
+        topBar = {
+            AffirmationTopBar()
+        }
+    ) { it ->
+        LazyColumn(contentPadding = it, modifier = modifier) {
+            items(affirmationList) { affirmation ->
+                AffirmationCard(
+                    affirmation = affirmation,
+                    modifier = Modifier.padding(8.dp)
+                )
+            }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AffirmationTopBar(modifier: Modifier= Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    modifier = Modifier.size(50.dp)
+                        .padding(16.dp),
+                    contentDescription = null,
+                    painter = painterResource(R.drawable.ic_launcher_background)
+                )
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        },
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    AffirmationAppTheme {
+    AffirmationAppTheme(darkTheme = true) {
         AffirmationsApp()
     }
 }
--- app/src/main/java/com/shreejacreation/affirmationapp/ui/AffirmationCard.kt
+++ app/src/main/java/com/shreejacreation/affirmationapp/ui/AffirmationCard.kt
@@ -1,9 +1,7 @@
 package com.shreejacreation.affirmationapp.ui
 
-import android.content.res.Resources
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -12,15 +10,16 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.compose.AffirmationAppTheme
 import com.shreejacreation.affirmationapp.R
 import com.shreejacreation.affirmationapp.data.Affirmation
-import com.shreejacreation.affirmationapp.ui.theme.AffirmationAppTheme
 
 @Composable
 fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
@@ -29,8 +28,9 @@ fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
             Image(
                 painter = painterResource(affirmation.imageResourceId),
                 contentDescription = stringResource(affirmation.stringResourceId),
-                modifier = Modifier.fillMaxWidth().height(194.dp),
-                contentScale = ContentScale.Crop
+                modifier = Modifier.fillMaxWidth().height(194.dp)
+                    .clip(MaterialTheme.shapes.small),
+                contentScale = ContentScale.Crop,
             )
             Text(
                 text = LocalContext.current.getString(affirmation.stringResourceId),
